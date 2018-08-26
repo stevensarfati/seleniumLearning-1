3f27256052ed36dcc0f064919182e8284ab61a5a
Dpinto09 <42576010+Dpinto09@users.noreply.github.com>
2018-08-26 15:32:50 +0300

    added

--- secondSeleniumProjectWithGetHub/src/test/java/EbayTest.java
+++ secondSeleniumProjectWithGetHub/src/test/java/EbayTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -14,14 +15,58 @@ public class EbayTest{
 
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        
-        driver.get("https://www.ebay.com/");
+
+        openEbay();
+        intitNewUserRegistration();
+        fillFirstNameForm();
+        fillLatsNameForm();
+        fillEmailForm();
+        fillPasswordForm();
+        clickOnButtonSubmitRegistration();
+
         driver.quit();
     }
 
+    public void openEbay() {
+        driver.get("https://www.ebay.com/");
+    }
 
+    public void intitNewUserRegistration() {
+        driver.findElement(By.linkText("register")).click();
+    }
 
+    //fill click on button
+    public void clickOnButtonSubmitRegistration() {
+        driver.findElement(By.id("ppaFormSbtBtn")).click();
+    }
 
+    // fill password field while registration
+    public void fillPasswordForm() {
+        driver.findElement(By.id("PASSWORD")).click();
+        driver.findElement(By.id("PASSWORD")).clear();
+        driver.findElement(By.id("PASSWORD")).sendKeys("D4C3B2A1a1b2c3d4");
+    }
+
+    // fill email field while registration
+    public void fillEmailForm() {
+        driver.findElement(By.id("email")).click();
+        driver.findElement(By.id("email")).clear();
+        driver.findElement(By.id("email")).sendKeys("bouchadpinto.david@outlook.com");
+    }
+
+    // fill lastname field while registration
+    public void fillLatsNameForm() {
+        driver.findElement(By.name("lastname")).click();
+        driver.findElement(By.name("lastname")).clear();
+        driver.findElement(By.name("lastname")).sendKeys("Pinto");
+    }
+
+    // fill firstname field while registration
+    public void fillFirstNameForm() {
+        driver.findElement(By.name("firstname")).click();
+        driver.findElement(By.name("firstname")).clear();
+        driver.findElement(By.name("firstname")).sendKeys("David");
+    }
 
 
 }
